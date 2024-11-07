736ed3d3fe99ac694cdff68879a464b979e4b733
Dhruv Patel <dhruv10203@gmail.com>
2024-11-06 19:20:23 -0500

    update logo for metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
   title: "Lifomation Waitlist",
   description:
     "Future of Document Management",
+  icons: {
+    icon: "/logo.png",
+  }
 };
 
 export default function RootLayout({
@@ -19,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="/opengraph-image.png" />
+      {/* <meta property="og:image" content="/opengraph-image.png" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="832" />
@@ -30,11 +33,14 @@ export default function RootLayout({
       <meta
         property="og:url"
         // content="https://nextjs-notion-waitlist.vercel.app/"
-      />
-      <meta name="twitter:image" content="/twitter-image.png" />
+      /> */}
+      {/* <meta name="twitter:image" content="/twitter-image.png" />
       <meta name="twitter:image:type" content="image/png" />
       <meta name="twitter:image:width" content="1280" />
-      <meta name="twitter:image:height" content="832" />
+      <meta name="twitter:image:height" content="832" /> */}
+      <head>
+      <link rel="icon" href="/logo.png" />
+      </head>
       <body className={FigtreeFont.className}>
         {children}
         <Toaster richColors position="top-center" />
